50911750fd6f78c8a2c82dbedf89c578f179f4f5
Joaquin <joaco.aravena02@gmail.com>
2022-10-17 22:27:13 -0300

    primer commit

--- colacp.c
+++ colacp.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+#define FALSE 0;
+#define TRUE 1;
+#define CCP_NO_INI 2;
+#define POS_NULA NULL;
+#define ELE_NULO NULL;
+
 typedef void * TClave;
 typedef void * TValor;
 
@@ -34,40 +41,55 @@ TColaCP crearColaCp(int (*f)(TEntrada, TEntrada)){
     TColaCP cola = (TColaCP)malloc(sizeof(struct cola_con_prioridad));
     TNodo raiz = (TNodo)malloc(sizeof(struct TNodo));
     cola->raiz = raiz;
+    cola->raiz->padre = POS_NULA;
     cola->cantidad_elementos = 0;
     cola->comparador = f;
     return cola;
 }
+void insertarRec(TColaCP cola, TNodo actual){
+    cola->raiz = actual;
+}
+
 int cpInsertar(TColaCP cola, TEntrada entr){
     if(cpCantidad(cola) == 0){
         cola->raiz->entrada = entr;
         cola->cantidad_elementos++;
         return 1;
     }else{
-        int inserte = 0;
-        TNodo actual = cola->raiz;
-        while(!inserte){
-            int comp = cola->comparador(entr, actual->entrada);
-            if(comp == 0)
-                return 0;
-            else if (comp == -1)
-                actual = actual->hijo_derecho;
-            else
-                actual = actual->hijo_izquierdo;
-            if(actual->entrada == NULL){
-                actual->entrada = entr;
-                cola->cantidad_elementos++;
-                return 1;
+        //insertar en la ult posicion
+        //ir chequeando si la prioridad del padre es mayor a la actual
+        TNodo nuevo = malloc(sizeof(struct TNodo));
+        nuevo->entrada = entr;
+        insertarRec(cola, nuevo);
+        while((nuevo->padre != NULL) && cola->comparador(nuevo->padre->entrada, nuevo->entrada) == 1){
+            //swap de nuevo y padre
+            TNodo padreAnterior = nuevo->padre;
+            nuevo->padre = nuevo->padre->padre; //es el ancestro
+            padreAnterior->padre = nuevo;
+            if(nuevo->padre->hijo_derecho == nuevo){
+                //es el hd
+                nuevo->padre->hijo_derecho = nuevo;
+                nuevo->hijo_derecho = padreAnterior;
+                nuevo->hijo_izquierdo = padreAnterior->hijo_izquierdo;
+            }else{
+                //es el hi
+                nuevo->padre->hijo_izquierdo = nuevo;
+                nuevo->hijo_izquierdo = padreAnterior;
+                nuevo->hijo_derecho = padreAnterior->hijo_derecho;
             }
+            padreAnterior->hijo_derecho = ELE_NULO;
+            padreAnterior->hijo_izquierdo = ELE_NULO;
         }
-        return 0;
+        return 1;
     }
+    return 0;
 }
+
 TEntrada cpEliminar(TColaCP cola){
     TEntrada ret = cola->raiz->entrada;
     TNodo aux = cola->raiz;
     if(cola->cantidad_elementos == 1){
-        cola->raiz = NULL;
+        cola->raiz = POS_NULA;
         cola->cantidad_elementos--;
     }else{
         cola->raiz = cola->raiz->hijo_derecho;
@@ -81,12 +103,9 @@ int cpCantidad(TColaCP cola){
 }
 void cpDestruir(TColaCP cola, void (*fEliminar)(TEntrada)){
     while(cola->cantidad_elementos != 0){
+        //empezar por hi de la raiz hasta q no tenga hijos
         fEliminar(cola->raiz->entrada);
         cola->raiz = cola->raiz->hijo_derecho;
     }
 }
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
-}
+
--- planificador.c
+++ planificador.c
@@ -1,9 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define FALSE 0;
+#define TRUE 1;
+
 typedef struct ciudad {
     char * nombre;
     float pos_x;
     float pos_y;
 } * TCiudad;
+
+void mostrarAscendente();
+void mostrarDescendente();
+void reducirHorasManejo();
+void salir(); //debo liberar o se libera solo?
+
+main(){
+    int termino; termino = FALSE;
+    do{
+        printf("Ingrese el numero de la operacion que desee\n");
+        printf("1. Mostrar las ciudades por visitar en orden ascendente\n");
+        printf("2. Mostrar las ciudades por visitar en orden descendente\n");
+        printf("3. Reducir horas de manejo");
+        printf("4. Salir");
+        int opcion;
+        scanf("%d", &opcion);
+        switch(opcion){
+            case 1: mostrarAscendente(); break;
+            case 2: mostrarDescendente(); break;
+            case 3: reducirHorasManejo(); break;
+            case 4: salir(); termino = TRUE; break;
+            default: printf("El numero ingresado no es valido");
+        }
+    }while (termino == FALSE);
+    return 0;
+}
 
